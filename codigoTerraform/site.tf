43d59922abccc73450c3493236ce95e3b1b24bfc
Lucas Gonçalves <lucastikon@gmail.com>
2026-04-19 23:45:46 +0000

    Update Terraform config and add .gitignore to ignore state files

--- codigoTerraform/site.tf
+++ codigoTerraform/site.tf
@@ -23,13 +23,12 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_storage_account" "site" {
-  name                      = "staccdevopsrg21605"
+  name                      = "staccdevopsrg2357890"
   resource_group_name       = azurerm_resource_group.rg.name
   location                  = azurerm_resource_group.rg.location
   account_kind              = "StorageV2"
   account_tier              = "Standard"
   account_replication_type  = "LRS"
-  enable_https_traffic_only = true
 
   static_website {
     index_document = "index.html"
